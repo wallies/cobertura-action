54ba52922ef2cd10f428fb438088df70745c0714
David <dib542@gmail.com>
2022-01-06 13:27:57 +1000

    Remove unused style tag

--- src/action.js
+++ src/action.js
@@ -242,7 +242,7 @@ function markdownReport(reports, commit, options) {
             // file detail row
             : `<tr><td>${row.filter(Boolean).join('</td><td align="center">')}</td></tr>`
           // folder name row
-          : `</tbody>\n<tbody>\n<tr><td colspan="10" style="border:none;"><br/>${row}</td></tr>\n</tbody>\n<tbody>`;
+          : `</tbody>\n<tbody>\n<tr><td colspan="10"><br/>${row}</td></tr>\n</tbody>\n<tbody>`;
       })
       .join("\n");
     const titleText = `<strong>${reportName}${folder}</strong>`;
