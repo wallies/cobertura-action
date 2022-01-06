f6ba92f3eda473ab13fa4b4cb68752356e6823c6
David <dib542@gmail.com>
2022-01-06 17:39:39 +1000

    Fix breaking hyphends in a different way

--- src/action.js
+++ src/action.js
@@ -111,7 +111,7 @@ function formatFileUrl(sourceDir, fileName, commit) {
 }
 
 function formatRangeText([start, end]) {
-  return `${start}` + (start === end ? "" : `-${end}`);
+  return `${start}` + (start === end ? "" : `-&NoBreak;${end}`);
 }
 
 function tickWrap(string) {
@@ -131,7 +131,7 @@ function cropRangeList(separator, showMissingMaxLength, ranges) {
 }
 
 function linkRange(fileUrl, range) {
-  const [start, end] = range.slice(1, -1).split("-", 2);
+  const [start, end] = range.split('&NoBreak;').join().slice(1, -1).split("-", 2);
   const rangeReference = `L${start}` + (end ? `-L${end}` : "");
   // Insert plain=1 to disabled rendered views.
   const url = `${fileUrl}?plain=1#${rangeReference}`;
